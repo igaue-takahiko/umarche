94b3751d754a158f974851a7cdd3abae83742282
igaue-takahhiko <hasewo.2610@gmail.com>
2021-06-27 14:48:58 +0900

    learn name with slot

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -1,3 +1,6 @@
 <x-tests.app>
+    <x-slot name="header">
+        header1
+    </x-slot>
     <h2>test1</h2>
 </x-tests.app>
--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -1,3 +1,6 @@
 <x-tests.app>
+    <x-slot name="header">
+        header2
+    </x-slot>
     <h2>test2</h2>
 </x-tests.app>
