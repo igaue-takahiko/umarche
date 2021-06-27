f06f4c8606d5a6f3473f5d666d3b49a42f3cfd6a
igaue-takahhiko <hasewo.2610@gmail.com>
2021-06-27 18:07:55 +0900

    learn component class base

--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -2,5 +2,7 @@
     <x-slot name="header">
         header2
     </x-slot>
-    <h2>test2</h2>
+    <x-test-class-base classBaseMessage="メッセージです" />
+    <div class="mb-3"></div>
+    <x-test-class-base classBaseMessage="メッセージです" defaultMessage="初期値から変更しています。" />
 </x-tests.app>
