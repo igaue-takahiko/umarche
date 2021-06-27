7fb2c63fdb5da13a7e150a2c725289f73ff3c23d
igaue-takahhiko <hasewo.2610@gmail.com>
2021-06-27 15:28:53 +0900

    component attribute bag

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -5,4 +5,5 @@
     <h2>test1</h2>
 <x-tests.card title="タイトル" content="本文です。" :message="$message" />
 <x-tests.card title="タイトル2" />
+<x-tests.card title="CSSを変更したい" class="bg-red-300" />
 </x-tests.card>
