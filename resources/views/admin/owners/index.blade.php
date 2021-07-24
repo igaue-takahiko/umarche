0444cb8a7559c467beea875f27ea791ae85dda52
igaue-takahhiko <hasewo.2610@gmail.com>
2021-07-25 08:54:10 +0900

    owner store

--- resources/views/admin/owners/index.blade.php
+++ resources/views/admin/owners/index.blade.php
@@ -11,6 +11,7 @@
                 <div class="p-6 bg-white border-b border-gray-200">
                     <section class="text-gray-600 body-font">
                         <div class="container px-5 mx-auto">
+                            <x-flash-message status="info" />
                             <div class="flex justify-end mb-4">
                                 <button class="px-8 py-2 text-lg text-white bg-blue-500 border-0 rounded focus:outline-none hover:bg-blue-600" onclick="location.href = '{{ route('admin.owners.create') }}'">新規登録する</button>
                             </div>
