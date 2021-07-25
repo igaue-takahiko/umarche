1cf0d3e0efdfd08085df26a3b7d198833a7c6da1
igaue-takahhiko <hasewo.2610@gmail.com>
2021-07-25 21:55:04 +0900

    Edit view and route

--- resources/views/admin/owners/index.blade.php
+++ resources/views/admin/owners/index.blade.php
@@ -8,9 +8,9 @@
     <div class="py-12">
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
+                <div class="bg-white border-b border-gray-200 md:p-6">
                     <section class="text-gray-600 body-font">
-                        <div class="container px-5 mx-auto">
+                        <div class="container mx-auto md:px-5">
                             <x-flash-message status="session('status')" />
                             <div class="flex justify-end mb-4">
                                 <button class="px-8 py-2 text-lg text-white bg-blue-500 border-0 rounded focus:outline-none hover:bg-blue-600" onclick="location.href = '{{ route('admin.owners.create') }}'">新規登録する</button>
@@ -19,26 +19,26 @@
                                 <table class="w-full text-left whitespace-no-wrap table-auto">
                                     <thead>
                                         <tr>
-                                            <th class="px-4 py-3 text-sm font-medium tracking-wider text-gray-900 bg-gray-100 rounded-tl rounded-bl title-font">名前</th>
-                                            <th class="px-4 py-3 text-sm font-medium tracking-wider text-gray-900 bg-gray-100 title-font">メールアドレス</th>
-                                            <th class="px-4 py-3 text-sm font-medium tracking-wider text-gray-900 bg-gray-100 title-font">作成日</th>
-                                            <th class="px-4 py-3 text-sm font-medium tracking-wider text-gray-900 bg-gray-100 rounded-tr rounded-br title-font"></th>
-                                            <th class="px-4 py-3 text-sm font-medium tracking-wider text-gray-900 bg-gray-100 rounded-tr rounded-br title-font"></th>
+                                            <th class="py-3 text-sm font-medium tracking-wider text-gray-900 bg-gray-100 rounded-tl rounded-bl md:px-4 title-font">名前</th>
+                                            <th class="py-3 text-sm font-medium tracking-wider text-gray-900 bg-gray-100 md:px-4 title-font">メールアドレス</th>
+                                            <th class="py-3 text-sm font-medium tracking-wider text-gray-900 bg-gray-100 md:px-4 title-font">作成日</th>
+                                            <th class="py-3 text-sm font-medium tracking-wider text-gray-900 bg-gray-100 rounded-tr rounded-br md:px-4 title-font"></th>
+                                            <th class="py-3 text-sm font-medium tracking-wider text-gray-900 bg-gray-100 rounded-tr rounded-br md:px-4 title-font"></th>
                                         </tr>
                                     </thead>
                                     <tbody>
                                         @foreach ($owners as $owner)
                                             <tr>
-                                                <td class="px-4 py-3">{{ $owner->name }}</td>
-                                                <td class="px-4 py-3">{{ $owner->email }}</td>
-                                                <td class="px-4 py-3 text-lg text-gray-900">{{ $owner->created_at->diffForHumans() }}</td>
-                                                <td class="px-4 py-3">
+                                                <td class="py-3 md:px-4">{{ $owner->name }}</td>
+                                                <td class="py-3 md:px-4">{{ $owner->email }}</td>
+                                                <td class="py-3 text-lg text-gray-900 md:px-4">{{ $owner->created_at->diffForHumans() }}</td>
+                                                <td class="py-3 md:px-4">
                                                     <button onclick="location.href = '{{ route('admin.owners.edit', [ 'owner' => $owner->id ]) }}'" class="px-4 py-2 text-white bg-blue-600 border-0 rounded focus:outline-none hover:bg-blue-700">編集</button>
                                                 </td>
                                                 <form id="delete_{{ $owner->id }}" action="{{ route('admin.owners.destroy', ['owner' => $owner->id]) }}" method="POST">
                                                     @csrf
                                                     @method('delete')
-                                                    <td class="px-4 py-3">
+                                                    <td class="py-3 md:px-4">
                                                         <a href="#" data-id="{{ $owner->id }}" onclick="deletePost(this)" class="px-4 py-2 text-white bg-red-600 border-0 rounded focus:outline-none hover:bg-red-700">削除</あ>
                                                     </td>
                                                 </form>
