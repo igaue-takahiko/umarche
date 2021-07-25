f1fb8a7055a4b7b0e0f9ebdd4a3d316850656379
igaue-takahhiko <hasewo.2610@gmail.com>
2021-07-25 15:18:36 +0900

    softDelete

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -68,7 +68,10 @@ class OwnersController extends Controller
 
         return redirect()
         ->route(('admin.owners.index'))
-        ->with('message', 'オーナー登録を実施しました。');
+        ->with([
+            'message' => 'オーナー登録を実施しました。',
+            'status' => 'info',
+        ]);
     }
 
     /**
@@ -112,7 +115,10 @@ class OwnersController extends Controller
 
         return redirect()
         ->route('admin.owners.index')
-        ->with('message', 'オーナー情報を更新しましました。');
+        ->with([
+            'message' => 'オーナー情報を更新しましました。',
+            'status' => 'info',
+        ]);
     }
 
     /**
@@ -123,6 +129,30 @@ class OwnersController extends Controller
      */
     public function destroy($id)
     {
-        //
+        Owner::findOrFail($id)->delete();
+
+        return redirect()
+        ->route(('admin.owners.index'))
+        ->with([
+            'message' => 'オーナー情報を削除しました。',
+            'status' => 'alert',
+        ]);
+    }
+
+    public function expiredOwnerIndex()
+    {
+        $expiredOwners = Owner::onlyTrashed()->get();
+        return view('admin.expired-owners', compact('expiredOwners'));
+    }
+
+    public function expiredOwnerDestroy($id)
+    {
+        Owner::onlyTrashed()->findOrFail($id)->forceDelete();
+        return redirect()
+        ->route('admin.expired-owners.index')
+        ->with([
+            'message' => 'オーナー情報を削除しました。',
+            'status' => 'alert',
+        ]);
     }
 }
--- resources/views/admin/expired-owners.blade.php
+++ resources/views/admin/expired-owners.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="text-xl font-semibold leading-tight text-gray-800">
-            オーナー一覧
+            期限切れオーナー一覧
         </h2>
     </x-slot>
 
@@ -12,34 +12,26 @@
                     <section class="text-gray-600 body-font">
                         <div class="container px-5 mx-auto">
                             <x-flash-message status="session('status')" />
-                            <div class="flex justify-end mb-4">
-                                <button class="px-8 py-2 text-lg text-white bg-blue-500 border-0 rounded focus:outline-none hover:bg-blue-600" onclick="location.href = '{{ route('admin.owners.create') }}'">新規登録する</button>
-                            </div>
                             <div class="w-full mx-auto overflow-auto lg:w-2/3">
                                 <table class="w-full text-left whitespace-no-wrap table-auto">
                                     <thead>
                                         <tr>
                                             <th class="px-4 py-3 text-sm font-medium tracking-wider text-gray-900 bg-gray-100 rounded-tl rounded-bl title-font">名前</th>
                                             <th class="px-4 py-3 text-sm font-medium tracking-wider text-gray-900 bg-gray-100 title-font">メールアドレス</th>
-                                            <th class="px-4 py-3 text-sm font-medium tracking-wider text-gray-900 bg-gray-100 title-font">作成日</th>
-                                            <th class="px-4 py-3 text-sm font-medium tracking-wider text-gray-900 bg-gray-100 rounded-tr rounded-br title-font"></th>
+                                            <th class="px-4 py-3 text-sm font-medium tracking-wider text-gray-900 bg-gray-100 title-font">期限切れ日</th>
                                             <th class="px-4 py-3 text-sm font-medium tracking-wider text-gray-900 bg-gray-100 rounded-tr rounded-br title-font"></th>
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        @foreach ($owners as $owner)
+                                        @foreach ($expiredOwners as $owner)
                                             <tr>
                                                 <td class="px-4 py-3">{{ $owner->name }}</td>
                                                 <td class="px-4 py-3">{{ $owner->email }}</td>
-                                                <td class="px-4 py-3 text-lg text-gray-900">{{ $owner->created_at->diffForHumans() }}</td>
-                                                <td class="px-4 py-3">
-                                                    <button onclick="location.href = '{{ route('admin.owners.edit', [ 'owner' => $owner->id ]) }}'" class="px-4 py-2 text-white bg-blue-600 border-0 rounded focus:outline-none hover:bg-blue-700">編集</button>
-                                                </td>
-                                                <form id="delete_{{ $owner->id }}" action="{{ route('admin.owners.destroy', ['owner' => $owner->id]) }}" method="POST">
+                                                <td class="px-4 py-3 text-lg text-gray-900">{{ $owner->deleted_at->diffForHumans() }}</td>
+                                                <form id="delete_{{ $owner->id }}" action="{{ route('admin.expired-owners.destroy' , ['owner' => $owner->id]) }}" method="POST">
                                                     @csrf
-                                                    @method('delete')
                                                     <td class="px-4 py-3">
-                                                        <a href="#" data-id="{{ $owner->id }}" onclick="deletePost(this)" class="px-4 py-2 text-white bg-red-600 border-0 rounded focus:outline-none hover:bg-red-700">削除</あ>
+                                                        <a href="#" data-id="{{ $owner->id }}" onclick="deletePost(this)" class="px-4 py-2 text-white bg-red-600 border-0 rounded focus:outline-none hover:bg-red-700">完全に削除</あ>
                                                     </td>
                                                 </form>
                                             </tr>
@@ -56,17 +48,6 @@
                             </div>
                         </div>
                     </section>
-                    {{-- エロクアント
-                    @foreach ($e_all as $e_owner)
-                        {{ $e_owner->name }}
-                        {{ $e_owner->created_at->diffForHumans() }}
-                    @endforeach
-                    <br/>
-                    クエリビルダ
-                    @foreach ($q_get as $q_owner)
-                        {{ $q_owner->name }}
-                        {{ Carbon\Carbon::parse($q_owner->created_at)->diffForHumans() }}
-                    @endforeach --}}
                 </div>
             </div>
         </div>
